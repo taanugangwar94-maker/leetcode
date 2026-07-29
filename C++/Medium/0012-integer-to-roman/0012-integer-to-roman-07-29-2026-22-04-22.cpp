6844e6f19e83d4965f3b323eff9eb4123adb8ecf
Taanu Gangwar <taanugangwar94@gmail.com>
2026-07-29 22:04:22 +0530

    Hash Table .cpp 0012-integer-to-roman

---
+++ C++/Medium/0012-integer-to-roman/0012-integer-to-roman-07-29-2026-22-04-22.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int> nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        string ans="";
+            for(int i=0;i<13;i++){
+                while(num>=nums[i]){
+                     ans+=roman[i];
+                    num-=nums[i];
+                }
+            }
+        return ans;
+    }
+};
