cb454802dc9d6e6391130af7aa53ff0217e9230e
Taanu Gangwar <taanugangwar94@gmail.com>
2026-07-31 10:13:28 +0530

    Array .cpp 0349-intersection-of-two-arrays

---
+++ C++/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays-07-31-2026-10-13-27.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>freq_nums1(1001,0);
+        vector<int>freq_nums2(1001,0);
+        for(int i=0;i<nums1.size();i++){
+            freq_nums1[nums1[i]]++;
+        }
+        for(int j=0;j<nums2.size();j++){
+            freq_nums2[nums2[j]]++;
+        }
+        vector<int>ans;
+        for(int i=0;i<=1000;i++){
+            if(freq_nums1[i]>0 && freq_nums2[i]>0){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
