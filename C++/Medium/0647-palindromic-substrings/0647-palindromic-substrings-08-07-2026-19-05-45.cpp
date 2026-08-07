1924d5cab5ced202f9ced4b2f9c184e51568c3f4
Taanu Gangwar <taanugangwar94@gmail.com>
2026-08-07 19:05:46 +0530

    Two Pointers .cpp 0647-palindromic-substrings

---
+++ C++/Medium/0647-palindromic-substrings/0647-palindromic-substrings-08-07-2026-19-05-45.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int expandAroundindex(const  string &str,int i,int j){
+        int count=0;
+        while(i>=0 && j<str.length() && str[i]==str[j]){
+            count++;
+            i--;
+            j++;
+        }
+
+        return count;
+    } 
+
+    int countSubstrings(string s) {
+        int totalcount=0;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            int oddstring=expandAroundindex(s,i,i);
+            totalcount=totalcount+oddstring;
+            int evenstring=expandAroundindex(s,i,i+1);
+            totalcount=totalcount+evenstring;
+        }
+        return totalcount;
+    }
+};
