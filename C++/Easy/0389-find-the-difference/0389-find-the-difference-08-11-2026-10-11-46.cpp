cf3e0c0068dfedb2455b6daaa331228223fd0f00
Taanu Gangwar <taanugangwar94@gmail.com>
2026-08-11 10:11:46 +0530

    Hash Table .cpp 0389-find-the-difference

---
+++ C++/Easy/0389-find-the-difference/0389-find-the-difference-08-11-2026-10-11-46.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,char>hashmap;
+        for(int i=0;i<s.size();i++ ){
+            hashmap[s[i]]+=1;
+        }
+        for(int i=0;i<t.size();i++){
+            if(hashmap[t[i]]==0){
+               return t[i];
+            }
+            hashmap[t[i]]--;
+        }
+    return { };
+    }
+};
