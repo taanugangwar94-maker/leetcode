205c23974da1b2856959de5e237e0741651d7362
Taanu Gangwar <taanugangwar94@gmail.com>
2026-08-13 21:58:02 +0530

    Array .cpp 0049-group-anagrams

---
+++ C++/Medium/0049-group-anagrams/0049-group-anagrams-08-13-2026-21-58-02.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string ,vector<string>>mp;
+        for(int i=0;i<strs.size();i++){
+            string s= strs[i];
+            sort(s.begin(),s.end());
+            mp[s].push_back(strs[i]);
+        }
+        vector<vector<string>>ans;
+        for (auto pair : mp) {
+            ans.push_back(pair.second);
+        }
+        return ans;
+    }
+};
