ddb7fdeb60c0ba1d10368d9acb79255e39153e14
Taanu Gangwar <taanugangwar94@gmail.com>
2026-08-14 10:23:28 +0530

    Array .cpp 0599-minimum-index-sum-of-two-lists

---
+++ C++/Easy/0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists-08-14-2026-10-23-27.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        unordered_map<string,int>mp;
+        vector<string>result;
+        int min_sum=INT_MAX;
+        for(int i=0;i<list1.size();i++){
+            mp[list1[i]]=i;
+        } 
+        for(int j=0;j<list2.size();j++){
+            if(mp.count(list2[j])){
+                int current_sum=mp[list2[j]]+j;
+                if(current_sum<min_sum){
+                    min_sum=current_sum;
+                    result.clear();
+                    result.push_back(list2[j]);
+                }else if(current_sum==min_sum){
+                    result.push_back(list2[j]);
+                }
+            }
+        }
+        return result;
+    }
+};
