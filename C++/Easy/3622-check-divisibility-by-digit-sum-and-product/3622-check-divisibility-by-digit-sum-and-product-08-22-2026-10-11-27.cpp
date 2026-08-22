dac795e39df25724219ebea130d2e492003ac9b0
Taanu Gangwar <taanugangwar94@gmail.com>
2026-08-22 10:11:28 +0530

    Math .cpp 3622-check-divisibility-by-digit-sum-and-product

---
+++ C++/Easy/3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product-08-22-2026-10-11-27.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int sum=0;
+        int product=1;
+        int temp=n;
+        while(temp){
+          sum+=temp%10;
+          product*=temp%10;
+          temp/=10;
+        }
+        if(n%(sum+product)!=0){
+          return false;
+        }
+        return true;
+    }
+};
