6a5490b730121f52b054c8071d1d55b0fc337ce8
Taanu Gangwar <taanugangwar94@gmail.com>
2026-08-28 23:41:33 +0530

    Math .cpp 0633-sum-of-square-numbers

---
+++ C++/Medium/0633-sum-of-square-numbers/0633-sum-of-square-numbers-08-28-2026-23-41-32.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+       long long i=0;
+       long long j=sqrt(c);
+        while(i<=j)
+      {
+          long long sum=i*i+j*j;
+          if(sum==c)
+          {
+            return true;
+          }else if(sum<c)
+          {
+            i++;
+          }else{
+            j--;
+          }
+
+      }
+      return false;
+    }
+
+};
